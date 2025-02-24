45fc0e7b450d3498510a7c5a3a255abdaa116ae9
Philip <anlanderp@gmail.com>
2025-02-24 11:51:18 +0100

    init

--- components/StoryblokProvider.tsx
+++ components/StoryblokProvider.tsx
@@ -2,10 +2,24 @@
 import { storyblokInit } from "@storyblok/react/rsc";
 import { PropsWithChildren } from "react";
 import page from "./page";
+import { HeroSection } from "./storyblok/hero-section";
+import { ImageSection } from "./storyblok/image-section";
+import { Space } from "./storyblok/space-section";
+import { CTA } from "./storyblok/cta-section";
+import { Columns } from "./storyblok/columns-section";
+import { ContactForm } from "./ui/form/contact-form";
+import { CardColumns } from "./storyblok/card-columns-section";
 
 storyblokInit({
   components: {
     page: page,
+    hero: HeroSection,
+    image_text: ImageSection,
+    space: Space,
+    CTA: CTA,
+    columns: Columns,
+    form: ContactForm,
+    card_columns: CardColumns
   },
 
   enableFallbackComponent: true,
